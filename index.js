e2c12494f8a5e70a609342f5b194528a621e5b8c
DESKTOP-R889J86\yangrui <youngray9706@gmail.com>
2026-06-06 20:31:18 +0800

    上传

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: YangRui
+ * @Date: 2026-05-26 21:51:19
+ * @LastEditors: YangRui
+ * @LastEditTime: 2026-06-06 20:26:18
+ * @Description: 请输入
+ */
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -6,7 +13,4 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(router)
 
-const PORT = process.env.PORT || 3000
-server.listen(PORT, () => {
-  console.log('JSON Server running')
-})
+module.exports = server
